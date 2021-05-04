8dda9c1698d13f3247576361aa2c1f6fd4ef0c04
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2021-05-04 14:27:15 -0700

    Update find_data.py

--- find_data.py
+++ find_data.py
@@ -8,7 +8,7 @@ Created on Sat May  1 18:53:16 2021
 
 
 def find_data(soup,part,clas):
-    return soup.find('part', {'class':'clas'})
+    return soup.find(part, {'class':clas})
 
 def get_text_strip(val):
     return val.get_text().strip()
